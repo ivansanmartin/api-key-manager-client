707571eaf68ee9f9c91612c0fa91027163141231
ivansanmartin <ivansanmartin987@gmail.com>
2025-01-22 00:24:49 -0300

    update: test

--- python-package/api_key_manager_client/api_key_manager.py
+++ python-package/api_key_manager_client/api_key_manager.py
@@ -4,7 +4,10 @@ class ApiKeyManagerClient:
     def __init__(self, api_url: str, api_reference_id: str, api_key_id: str, api_key: str):
         self.api_url = api_url
         response = requests.get(self.api_url)
-        print(response)
+        data = response.json()        
+        
+        print(data)
+        
         self.api_reference_id = api_reference_id
         self.api_key_id = api_key_id
         self.api_key = api_key
