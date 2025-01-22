63388e6a0e7a16348a2872a09bf4581b4776e698
ivansanmartin <ivansanmartin987@gmail.com>
2025-01-22 00:54:16 -0300

    update: package

--- python-package/build/lib/api_key_manager_client/api_key_manager.py
+++ python-package/build/lib/api_key_manager_client/api_key_manager.py
@@ -7,7 +7,7 @@ class ApiKeyManagerClient:
     
     def verify_api_key(self, api_reference_id: str, api_key_id: str, api_key: str):
         response = requests.post(f'{self.api_url}/api/{self.version}/api-key-manager/verify-key', 
-                                 {
+                                 json={
                                     "api_reference_id": api_reference_id,
                                     "api_key_id": api_key_id,
                                     "api_key": api_key
