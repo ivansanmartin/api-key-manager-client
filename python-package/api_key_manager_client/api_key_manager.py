c77635c65d419d28b16cb27cc532505cb9bcc12f
ivansanmartin <ivansanmartin987@gmail.com>
2025-03-14 00:35:36 -0300

    update: verify key

--- python-package/api_key_manager_client/api_key_manager.py
+++ python-package/api_key_manager_client/api_key_manager.py
@@ -5,13 +5,11 @@ class ApiKeyManagerClient:
         self.api_url = api_url    
         self.version = version
     
-    def verify_api_key(self, api_reference_id: str, api_key_id: str, api_key: str):
+    def verify_api_key(self, api_key: str):
         try:
             response = requests.post(
                 f'{self.api_url}/api/{self.version}/api-key-manager/verify-key',
                 json={
-                    "api_reference_id": api_reference_id,
-                    "api_key_id": api_key_id,
                     "api_key": api_key
                 }
             )
